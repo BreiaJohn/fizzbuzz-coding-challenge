6d36a469e630d1b337cfe791388e89d8629ee3f2
Breia <breiamj4@gmail.com>
2024-06-03 08:07:25 -0400

    update so fizzbuzz can properly excuted

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ function fizzbuzz(n) {
     const results = [];
 
     for (let i = 1; i<= n; i++) {
-        if (i % 3 === 0){
-            results.push('Fizz');
-        } else if (i % 5 === 0) {
-            results.push('Buzz');
-        } else if (i % 5 === 0 && i % 3 === 0) {
+        if (i % 3 === 0 && i % 5 === 0){
             results.push('FizzBuzz');
+        } else if (i % 3 === 0) {
+            results.push('fizz');
+        } else if (i% 5 === 0) {
+            results.push('Buzz');
         } else {
             results.push(i);
         }
